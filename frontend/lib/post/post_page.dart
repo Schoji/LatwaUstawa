bb62e25e185ce217cb30936b0e4179ffff9c956b
Piotr Wittig <96655599+Schoji@users.noreply.github.com>
2025-12-06 19:02:40 +0100

    Changed post_page and colors.

--- frontend/lib/post/post_page.dart
+++ frontend/lib/post/post_page.dart
@@ -5,6 +5,20 @@ import 'package:latwe_ustawy/models/post_model.dart';
 import 'package:latwe_ustawy/post/widgets/tag_badge.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 
+String fixHtmlList(String html) {
+  return html
+      .replaceAll(
+        '<ul>',
+        '<br><br>',
+      ) // Usuwamy otwarcie listy (to ono robi wcięcie)
+      .replaceAll('</ul>', '') // Usuwamy zamknięcie
+      .replaceAll('<li>', '• ') // Zamieniamy punkt listy na kropkę z paszczą
+      .replaceAll(
+        '</li>',
+        '<br><br>',
+      ); // Zamieniamy koniec punktu na nową linię (x2 żeby był odstęp)
+}
+
 class PostPage extends StatelessWidget {
   const PostPage({super.key, required this.post});
 
@@ -18,52 +32,93 @@ class PostPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              spacing: 10,
-              children: [
-                Row(
-                  spacing: 10,
-                  children: post.tags.map((tag) => TagBadge(tag: tag)).toList(),
-                ),
-                Text(
-                  post.title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: colors.surfaceContainerHigh,
-                    borderRadius: BorderRadius.circular(12),
+          child: Container(
+            decoration: BoxDecoration(
+              color: colors.surfaceContainerLow,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 20,
+                children: [
+                  Row(
+                    spacing: 10,
+                    children: post.tags
+                        .map((tag) => TagBadge(tag: tag))
+                        .toList(),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(post.representativeName),
-                            Text(
-                              "zaktualizowano ${DateFormat('d MMM yyyy').format(post.submissionDate)}",
-                              style: TextStyle(fontSize: 12),
+                  Text(
+                    post.title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: colors.surfaceContainerHigh,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(post.representativeName),
+                              Text(
+                                "zaktualizowano ${DateFormat('d MMM yyyy').format(post.submissionDate)}",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: colors.onSurfaceVariant,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            post.sourceId,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: colors.onSurfaceVariant,
                             ),
-                          ],
-                        ),
-                      ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                HtmlWidget(post.aiSummary),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    IconButton(onPressed: () {}, icon: Icon(LucideIcons.heart)),
-                    Text(post.likesCount.toString()),
-                  ],
-                ),
-              ],
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Opis",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      HtmlWidget(
+                        fixHtmlList(post.aiSummary),
+
+                        textStyle: TextStyle(
+                          fontSize: 15,
+                          height: 1.4,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  TextButton.icon(
+                    onPressed: () {},
+                    style: const ButtonStyle(
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    icon: Icon(LucideIcons.heart),
+                    label: Text(post.likesCount.toString()),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
